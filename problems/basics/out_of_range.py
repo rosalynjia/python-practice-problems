898efb644fd029cfaf672470f125e2e1136d02de
Rosalyn Jia <rosalynjia@uchicago.edu>
2026-02-21 20:06:35 -0600

    Implement range check in out_of_range function

--- problems/basics/out_of_range.py
+++ problems/basics/out_of_range.py
@@ -4,7 +4,7 @@ def out_of_range(x, lb, ub):
 
     ### YOUR CODE GOES HERE
     # Replace "None" with the correct expression
-    r = None
+    r = x < lb or x > ub
 
     ### DO NOT MODIFY THE FOLLOWING LINE!
     return r
